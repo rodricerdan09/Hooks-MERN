4b04dbcd75877ad422f06ec077c6da5afffb44f9
Rodrigo D. Cerdan <cerdanrodrigo.utn@gmail.com>
2020-11-23 05:24:12 -0300

    Section 03: Added changes about Events, useState and button handles

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 // usar rafcp para crear un archivo react con PropTypes
 
 const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState(value); // [value, function]
+
+    const handleAdd = () => {
+        // setCounter(counter +1);
+        setCounter( (counter) => counter + 1)
+    }
+    const handleReset = () => {
+        setCounter( (counter) => counter = value)
+    }
+    const handleSubstract = () => {
+        setCounter( (counter) => counter - 1)
+    }
+
     return(
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
+            {/* Se pasa la funcion por referencia  */}
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubstract }>-1</button>
         </>
     );
 };
