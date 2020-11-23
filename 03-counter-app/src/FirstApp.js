22597ce0dc95f1c29901a6ca161759eadfb20346
Rodrigo D. Cerdan <cerdanrodrigo.utn@gmail.com>
2020-11-23 04:41:06 -0300

    Section 03: Added changes about use of PropTypes and DefaultProps

--- 03-counter-app/src/FirstApp.js
+++ 03-counter-app/src/FirstApp.js
@@ -1,22 +1,27 @@
+
 import React from 'react';
+import PropTypes from 'prop-types';
 // import React, {Fragment} from 'react';
 
-const FirstApp = () => {
-
-    const greeting = 'Hello World';
-
-    // eslint-disable-next-line no-unused-vars
-    const greetingObject = {
-        name: 'Rodrigo',
-        surname: 'Cerdan'
-    }
+// const FirstApp = (props) => {  <h1> {props.greeting} </h1>}
+const FirstApp = ({greeting = 'Hello World', subtitle = 'I\'m a subtitle', paragraph }) => {
 
     return(
         <>
             <h1> {greeting} </h1>
             {/* <pre>Hi {JSON.stringify(greetingObject, null, 3)} </pre> */}
-            <p>First App</p>
+            <h3> {subtitle} </h3>
+            <p> {paragraph} </p>
         </>
     )
 }
+
+FirstApp.propTypes = {
+    greeting: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+    paragraph: 'I\'m a paragraph'
+}
+
 export default FirstApp;
